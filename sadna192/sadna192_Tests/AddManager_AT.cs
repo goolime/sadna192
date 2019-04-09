897a14e43b517df738c681c06f531fde3ba4c7d0
Gal Meir <goolime@gmail.com>
2019-04-09 23:11:00 +0300

    AddOwner_AT pass

--- sadna192/sadna192_Tests/AddManager_AT.cs
+++ sadna192/sadna192_Tests/AddManager_AT.cs
@@ -43,29 +43,29 @@ namespace sadna192.Tests
         public void add_shop_manager_and_check_permissions_test()
         {
             Assert.ThrowsException<Exception>(() => { userServiceLayer2.Add_Product_Store("mamma mia", "seven up", "drink", 7, 10, new noDiscount(), new regularPolicy()); }, "user2 still is not manager so he can't add products");
-            Assert.ThrowsException<Exception>(() => { userServiceLayer2.Update_Product_Store("mamma mia", "potato", "potato", "vegetable", 30, 5, new noDiscount(), new regularPolicy()); }, "user2 still is not manager so he can't update products");
+            Assert.ThrowsException<Exception>(() => { userServiceLayer2.Update_Product_Store("mamma mia", "potato", "chips potato", "food", 30, 5, new noDiscount(), new regularPolicy()); }, "user2 still is not manager so he can't update products");
             Assert.ThrowsException<Exception>(() => { userServiceLayer2.Remove_Product_Store("mamma mia", "potato1"); }, "user2 still is not manager so he can't remove products");
 
             Assert.IsTrue(userServiceLayer1.Add_Store_Manager("mamma mia", "addManagerUser2", true, false, false));
 
-            List<ProductInStore> search1 = userServiceLayer2.GlobalSearch("seven up", "drink", null, 0, 100, 0, 0);
+            List<ProductInStore> search1 = userServiceLayer2.GlobalSearch("seven up", null, null, -1, -1, -1, -1);
             int pre_amount = search1.Count;
             Assert.IsTrue(userServiceLayer2.Add_Product_Store("mamma mia", "seven up", "drink", 7, 10, new noDiscount(), new regularPolicy()));
-            search1 = userServiceLayer2.GlobalSearch("seven up", "drink", null, 0, 100, 0, 0);
+            search1 = userServiceLayer2.GlobalSearch("seven up", null, null, -1, -1, -1, -1);
             Assert.AreEqual(pre_amount + 1, search1.Count);
-            Assert.ThrowsException<Exception>(() => { userServiceLayer2.Update_Product_Store("mamma mia", "potato", " potato", "vegetable", 30, 9, new noDiscount(), new regularPolicy()); }, "user2 has no update permissions so he can't update products");
+            Assert.ThrowsException<Exception>(() => { userServiceLayer2.Update_Product_Store("mamma mia", "potato", "chips potato", "food", 30, 9, new noDiscount(), new regularPolicy()); }, "user2 has no update permissions so he can't update products");
             Assert.ThrowsException<Exception>(() => { userServiceLayer2.Remove_Product_Store("mamma mia", "potato"); }, "user2 has no remove permissions so he can't remove products");
+            
+           Assert.IsTrue(userServiceLayer1.Add_Store_Manager("mamma mia", "addManagerUser2", true, false, true));
 
-            Assert.IsTrue(userServiceLayer1.Add_Store_Manager("mamma mia", "addManagerUser2", true, true, false));
-
-            search1 = userServiceLayer2.GlobalSearch("potato", "food", null, 0, 100, 0, 0);
+            search1 = userServiceLayer2.GlobalSearch("potato", null, null, -1, -1, -1, -1);
             int pre_amount1 = search1.Count;
-            List<ProductInStore> search2 = userServiceLayer2.GlobalSearch("potato", "vegetable", null, 0, 100, 0, 0);
+            List<ProductInStore> search2 = userServiceLayer2.GlobalSearch("chips potato", null, null, -1, -1, -1, -1);
             int pre_amount2 = search2.Count;
-            Assert.IsTrue(userServiceLayer2.Update_Product_Store("mamma mia", "potato", " potato", "vegetable", 30, 9, new noDiscount(), new regularPolicy()));
-            search1 = userServiceLayer2.GlobalSearch("potato", "food", null, 0, 100, 0, 0);
+            Assert.IsTrue(userServiceLayer2.Update_Product_Store("mamma mia", "potato", "chips potato", "food", 30, 9, new noDiscount(), new regularPolicy()));
+            search1 = userServiceLayer2.GlobalSearch("potato", null, null, -1, -1, -1, -1);
             Assert.AreEqual(pre_amount1 - 1, search1.Count);
-            search2 = userServiceLayer2.GlobalSearch("potato", "vegetable", null, 0, 100, 0, 0);
+            search2 = userServiceLayer2.GlobalSearch("chips potato", null, null, -1, -1, -1, -1);
             Assert.AreEqual(pre_amount2 + 1, search2.Count);
             Assert.AreEqual(search2[0].getStore().getName(), "mamma mia");
             for (int i = 0; i < search1.Count; i++)
@@ -74,11 +74,11 @@ namespace sadna192.Tests
                     Assert.Fail("manager faild to update the product properly");
             }
             Assert.ThrowsException<Exception>(() => { userServiceLayer2.Remove_Product_Store("mamma mia", "potato"); }, "user2 has no remove permissions so he can't remove products");
-
+            
             Assert.IsTrue(userServiceLayer1.Add_Store_Manager("mamma mia", "addManagerUser2", true, true, true));
 
-            Assert.IsTrue(userServiceLayer2.Remove_Product_Store("mamma mia", "potato"));
-            search2 = userServiceLayer2.GlobalSearch("potato", "vegetable", null, 0, 100, 0, 0);
+            Assert.IsTrue(userServiceLayer2.Remove_Product_Store("mamma mia", "chips potato"));
+            search2 = userServiceLayer2.GlobalSearch("chips potato", null, null, -1, -1, -1, -1);
             Assert.AreEqual(pre_amount2 -1, search2.Count);
             for (int i = 0; i < search2.Count; i++)
             {
@@ -101,7 +101,7 @@ namespace sadna192.Tests
             {
                 userServiceLayer3.Login("addManagerUser3", "12345Fgbh");
                 if(userServiceLayer1.Add_Store_Manager("mamma mia", "addManagerUser3", true, true , true))
-                    Assert.ThrowsException<Exception>(() => { userServiceLayer1.Add_Store_Manager("mamma mia", "addManagerUser3", true, false, true); }, "the manager is already manager");
+                    Assert.ThrowsException<Exception>(() => { userServiceLayer1.Add_Store_Manager("mamma mia", "addManagerUser3", true, true, true); }, "the manager is already manager");
             }
         }
 
--- sadna192/sadna192_Tests/AddOwner_AT.cs
+++ sadna192/sadna192_Tests/AddOwner_AT.cs
@@ -20,16 +20,16 @@ namespace sadna192.Tests
         public void init()
         {
             serviceLayer = new ServiceLayer();
-            serviceLayer.Create_ServiceLayer(new stub_deliverySystem(), new stub_paymentSystem(), "admin", "1234");
+            serviceLayer.Create_ServiceLayer(new stub_deliverySystem(), new stub_paymentSystem(), "admin", "1234FgHY");
             userServiceLayer1 = serviceLayer.Connect();
             if (userServiceLayer1.Register("addOwneruser", "1221Abcd"))
                 if (userServiceLayer1.Login("addOwneruser", "1221Abcd"))
                 {
                     if (userServiceLayer1.Open_Store("shopipi"))
                     {
-                        userServiceLayer1.Add_Product_Store("shopipi", "cake", "food", 25, 4, null, null);
-                        userServiceLayer1.Add_Product_Store("shopipi", "water", "drink", 9, 100, null, null);
-                        userServiceLayer1.Add_Product_Store("shopipi", "chips", "food", 17, 50, null, null);
+                        userServiceLayer1.Add_Product_Store("shopipi", "cake", "food", 25, 4, new noDiscount(), new regularPolicy());
+                        userServiceLayer1.Add_Product_Store("shopipi", "water", "drink", 9, 100, new noDiscount(), new regularPolicy());
+                        userServiceLayer1.Add_Product_Store("shopipi", "chips", "food", 17, 50, new noDiscount(), new regularPolicy());
                     }
                 }
             userServiceLayer2 = serviceLayer.Connect();
@@ -42,27 +42,28 @@ namespace sadna192.Tests
         [TestMethod()]
         public void add_shop_owner_tests()
         {
-            Assert.ThrowsException<Exception>(() => { userServiceLayer2.Add_Product_Store("shopipi", "cola", "drink", 7, 10, null, null); }, "user2 still not owner so he can't add products");
-            Assert.ThrowsException<Exception>(() => { userServiceLayer2.Update_Product_Store("shopipi", "cake", "chocolate cake", "food", 30, 9, null, null); }, "user2 still not owner so he can't update products");
+            Assert.ThrowsException<Exception>(() => { userServiceLayer2.Add_Product_Store("shopipi", "cola", "drink", 7, 10, new noDiscount(), new regularPolicy()); }, "user2 still not owner so he can't add products");
+            Assert.ThrowsException<Exception>(() => { userServiceLayer2.Update_Product_Store("shopipi", "cake", "chocolate cake", "food", 30, 9, new noDiscount(), new regularPolicy()); }, "user2 still not owner so he can't update products");
             Assert.ThrowsException<Exception>(() => { userServiceLayer2.Remove_Product_Store("shopipi", "cake"); }, "user2 still not owner so he can't remove products");
 
             Assert.IsTrue(userServiceLayer1.Add_Store_Owner("shopipi", "addOwneruser2"));
 
-            List<ProductInStore> search1 = userServiceLayer2.GlobalSearch("cola", "drink", null, 0, 100, 0, 0);
+            List<ProductInStore> search1 = userServiceLayer2.GlobalSearch("cola", null, null, -1, -1, -1, -1);
             int pre_amount = search1.Count;
-            Assert.IsTrue(userServiceLayer2.Add_Product_Store("shopipi", "cola", "drink", 7, 10, null, null));
-            search1 = userServiceLayer2.GlobalSearch("cola", "drink", null, 0, 100, 0, 0);
+            Assert.IsTrue(userServiceLayer2.Add_Product_Store("shopipi", "cola", "drink", 7, 10, new noDiscount(), new regularPolicy()));
+            search1 = userServiceLayer2.GlobalSearch("cola", null, null, -1, -1, -1, -1);
             Assert.AreEqual(pre_amount + 1, search1.Count);
 
-            search1 = userServiceLayer2.GlobalSearch("cake", "food", null, 0, 100, 0, 0);
+            search1 = userServiceLayer2.GlobalSearch("cake", null, null, -1, -1, -1, -1);
             int cake_pre_amount = search1.Count;
-            List<ProductInStore> search2 = userServiceLayer2.GlobalSearch("chocolate cake", "food", null, 0, 100, 0, 0);
+            List<ProductInStore> search2 = userServiceLayer2.GlobalSearch("chocolate cake", null, null, -1, -1, -1, -1);
             int chocolate_cake_pre_amount = search2.Count;
-            Assert.IsTrue(userServiceLayer2.Update_Product_Store("shopipi", "cake", "chocolate cake", "food", 30, 9, null, null));
-            search1 = userServiceLayer2.GlobalSearch("cake", "food", null, 0, 100, 0, 0);
+            Assert.IsTrue(userServiceLayer2.Update_Product_Store("shopipi", "cake", "chocolate cake", "food", 30, 9, new noDiscount(), new regularPolicy()));
+            search1 = userServiceLayer2.GlobalSearch("cake", null, null, -1, -1, -1, -1);
             Assert.AreEqual(cake_pre_amount - 1, search1.Count);
-            search2 = userServiceLayer2.GlobalSearch("chocolate cake", "food", null, 0, 100, 0, 0);
+            search2 = userServiceLayer2.GlobalSearch("chocolate cake", null, null, -1, -1, -1, -1);
             Assert.AreEqual(chocolate_cake_pre_amount +1 , search2.Count);
+            chocolate_cake_pre_amount = search2.Count;
             Assert.AreEqual(search2[0].getStore().getName(), "shopipi");
             for (int i = 0; i < search1.Count; i++)
             {
@@ -71,20 +72,20 @@ namespace sadna192.Tests
             }
 
             Assert.IsTrue(userServiceLayer2.Remove_Product_Store("shopipi", "chocolate cake"));
-            search2 = userServiceLayer2.GlobalSearch("chocolate cake", "food", null, 0, 100, 0, 0);
+            search2 = userServiceLayer2.GlobalSearch("chocolate cake", null, null, -1, -1, -1, -1);
             Assert.AreEqual(chocolate_cake_pre_amount -1, search2.Count);
             for (int i = 0; i < search2.Count; i++)
             {
                 if (search2[i].getStore().getName().Equals("shopipi"))
                     Assert.Fail("owner faild to remove the product properly");
             }
-
+            
         }
 
         [TestMethod()]
         public void add_shop_owner_that_not_exist_test()
         {
-            Assert.ThrowsException<Exception>(() => { userServiceLayer1.Add_Store_Owner("shopipi", "user_not_exist"); }, "faild to add owner that not exist");
+            Assert.ThrowsException<Exception>(() => { userServiceLayer1.Add_Store_Owner("shopipi", "userNotExist"); }, "faild to add owner that not exist");
         }
 
         [TestMethod()]

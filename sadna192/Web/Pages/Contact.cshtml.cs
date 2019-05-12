ca103363509a470047622da769799713562a67ac
DvirR <devir0raviv@gmail.com>
2019-05-12 12:51:01 +0300

    added template Web

---
+++ sadna192/Web/Pages/Contact.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Pages
+{
+    public class ContactModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public ContactModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Customers.Add(Customer);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("/Index");
+        }
+    }
+}

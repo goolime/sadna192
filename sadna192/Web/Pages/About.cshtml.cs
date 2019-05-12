836acfef9c3f683baa4b4aadba356bb53c4c9304
DvirR <devir0raviv@gmail.com>
2019-05-12 12:51:01 +0300

    added template Web

---
+++ sadna192/Web/Pages/About.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+
+namespace Web.Pages
+{
+    public class AboutModel : PageModel
+    {
+        [ViewData]
+        public string Title { get; } = "About";
+
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            string directory = Environment.CurrentDirectory;
+            Message = String.Format("Your directory is {0}.", directory);
+        }
+    }
+}

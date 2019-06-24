6d867d4eb151363506ee6491ee7965af901f00a6
Gal Meir <goolime@gmail.com>
2019-06-24 12:35:48 +0300

    Auto stash before merge of "domain_changes" and "origin/domain_changes"

---
+++ sadna192/sadna192/Model1.cs
@@ -0,0 +1,21 @@
+﻿namespace sadna192
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+    using System.Data.Entity.ModelConfiguration.Conventions;
+
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        { }
+    }
+}
